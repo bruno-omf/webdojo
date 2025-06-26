a594fdb51d238df0a901742cfb3b65b87ffe2e4a
bruno-omf <bomf@cesar.school>
2025-06-26 15:53:16 -0300

    add consultancy initial test case

--- web/cypress/e2e/consultancy.cs.js
+++
@@ -1,9 +0,0 @@
-describe('Consultancy Form', ()=> {
-
-    it('Should request individual consultancy', ()=> {
-        cy.start() // pre-requisito
-        cy.submitLoginForm('papito@webdojo.com', 'katana123') // pre-requisito
-
-        
-    })
-})
