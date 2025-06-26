dde617f76d7eb07a688ac40365eac1e8a33cfdb7
Bruno Oliveira <brunodmff@gmail.com>
2025-06-26 14:04:22 -0300

    add initial cypress tests cases

---
+++ web/cypress/e2e/login.cy.js
@@ -0,0 +1,38 @@
+// describe('template spec', () => {
+//   it('passes', () => {
+//     cy.viewport(1440, 900)
+//     cy.visit('http://localhost:3000')
+//   })
+// })
+
+describe('Login', ()=>{ // cenário de login
+  it('Should log in successfully', ()=> { // caso de teste 1
+    cy.start()
+    cy.submitLogin('papito@webdojo.com', 'katana123')
+
+    cy.get('[data-cy="user-name"]')
+      .should('be.visible')
+      .and('have.text', 'Fernando Papito')
+
+    cy.get('[data-cy="welcome-message"]')
+      .should('be.visible')
+      .and('have.text', 'Olá QA, esse é o seu Dojo para aprender Automação de Testes.')
+  })
+
+  it('Should not log in with wrong password', ()=> { // caso de teste 2
+    cy.start()
+    cy.submitLogin('papito@webdojo.com', 'katana000')
+
+    cy.contains('Acesso negado! Tente novamente.')
+      .should('be.visible')
+  })
+
+  it('Should not log in with wrong email', ()=> { // caso de teste 3
+    cy.start()
+    cy.submitLogin('404@webdojo.com', 'katana123')
+
+    cy.contains('Acesso negado! Tente novamente.')
+      .should('be.visible')
+  })
+
+})
