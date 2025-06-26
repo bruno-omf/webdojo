dea358d60c292f98c275d18aa270d3c19e8357c6
bruno-omf <bomf@cesar.school>
2025-06-26 14:55:54 -0300

    add consultancy cypress file

---
+++ web/cypress/e2e/consultancy.cs.js
@@ -0,0 +1,9 @@
+describe('Consultancy Form', ()=> {
+
+    it('Should request individual consultancy', ()=> {
+        cy.start() // pre-requisito
+        cy.submitLoginForm('papito@webdojo.com', 'katana123') // pre-requisito
+
+        
+    })
+})
