57334f0250de2ff584073d6400094949c86f7167
Bruno Oliveira <brunodmff@gmail.com>
2025-07-07 15:16:00 -0300

    add forEach exercise and file input exercise

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -5,5 +5,77 @@ describe('Consultancy Form', () => {
         cy.submitLoginForm('papito@webdojo.com', 'katana123') // pre-condição
 
         cy.goTo('Formulários', 'Consultoria')
+
+        cy.get('input[placeholder="Digite seu nome completo"]').type('Bruno Oliveira')
+        cy.get('input[placeholder="Digite seu email"]').type('papito@webdojo.com')
+            // .should('have.value', 'papito@webdojo.com')
+        cy.contains('label', 'Telefone')
+            .parent()
+            .find('input').type('81988880000')
+            .should('have.value', ('(81) 98888-0000'))
+
+        cy.contains('label', 'Tipo de Consultoria')
+            .parent()
+            .find('select')
+            .select('Individual')
+
+        cy.contains('label', 'Pessoa Física')
+            .find('input')
+            .check()
+
+        cy.contains('label', 'CPF')
+            .parent()
+            .find('input')
+            .type('60936861088')
+            .should('have.value', '609.368.610-88')
+
+        const discoveryChannels = [
+            'Instagram',
+            'LinkedIn',
+            'Udemy',
+            'YouTube',
+            'Indicação de Amigo'
+        ]
+
+        discoveryChannels.forEach( (channel)=> {
+            cy.contains('label', channel)
+                .find('input')
+                .check()
+                .should('be.checked')
+        })
+            
+        cy.get('input[type="file"]')
+            .selectFile('./cypress/fixtures/document.pdf', {force: true})
+
+        // cy.get('input[placeholder="Digite seu nome completo"]').type('Fernando Papito') // selecionando um campo pelo placeholder, caso nao haja ID. selecionar a tag que representa o elemento.
+        // // cy.get('#email').type('papito@webdojo.com') // selecionando um campo pelo ID
+        // cy.get('input[placeholder="Digite seu email"]').type('papito@webdojo.com')
+        // cy.get('input[placeholder="(00) 00000-0000"]').type('81988880000')
+        //     .should('have.value', '(81) 98888-0000') // o should vai garantir que o formato do campo a ser preenchido conforme o esperado/solicitado do cliente
+
+        // // cy.get('#consultancyType').select('Individual')
+        // cy.contains('label', 'Tipo de Consultoria') // o label é um elemento que traz o título do campo que vai ser usado para a entrada de dados. 
+        //     .parent() // volto um "diretorio"
+        //     .find('select') // acho o elemento "select" dentro da div
+        //     .select('Individual') // seleciono a opcao "individual" do elemento "select"
+
+        // // cy.contains('span', 'Pessoa Física')
+        // //     .parent()
+        // //     .find('input')
+        // //     .check()
+        // cy.contains('label', 'Pessoa Física')
+        // .find('input')
+        // .check() // o click aqui também funcionaria para selecionar uma opçao de botao estilo 'radio'
+        // .should('be.checked') // garantia de que o botão radio de pessoa física está marcado
+
+        // cy.contains('label', 'Pessoa Jurídica')
+        // .find('input')
+        // .should('be.not.checked') // garantia de que o campo irá ficar desmarcado, já que estamos num caso de teste de pessoa física.
+
+        // cy.contains('label', 'CPF')
+        //     .parent()
+        //     .find('input')
+        //     .type('60936861088')
+        //     .should('have.value', '609.368.610-88')
     })
 })
